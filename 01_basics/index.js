151d47e805eddcfba1b6bab380ff26777ef43a88
rohitbhatt2437 <136969234+rohitbhatt2437@users.noreply.github.com>
2024-05-17 08:07:50 +0000

    completed some concepts

---
+++ 01_basics/index.js
@@ -0,0 +1,17 @@
+// const score = 400
+// console.log(score);
+
+// const balance = new Number(100)
+// console.log(balance.toFixed(2));
+
+// const othernumber = 1123.896
+// console.log(othernumber.toPrecision(4));
+
+// const number = 1000000
+// console.log(number.toLocaleString('en-IN'));
+// console.log(Math.random());
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()*(max-min+1))+min);
