8f4536db716932569c93c921fc8143fb52dd684b
rohitbhatt2437 <136969234+rohitbhatt2437@users.noreply.github.com>
2024-05-20 17:09:10 +0000

    done with this

---
+++ 01_basics/thisuse.js
@@ -0,0 +1,33 @@
+// const user={
+//     username:"Rohit bhatt",
+//     price: 999,
+//     welcome:function(){
+//         console.log(`${this.username}, welcome to the course`);
+//     }
+// }
+
+// user.welcome()
+// user.username="krish"
+// user.welcome()
+
+//IMPLICIT FUNCTION --> WHEN WE WRITE A FUCNTION WITHOUT RETURN STATEMENT, MAINLY USED IN ONE LINER FUNCTIONS
+// const addtwo=(num1, num2) => (num1+num2)
+// console.log(addtwo(9, 12))
+
+
+//EXPLICIT --> WHEN WE WRITE A FUNCTION WITH RETURN STATEMENT
+
+/////////////////IMMEDIATELY INVOKED FUNCTION/////////////////////////////////
+
+(function chaa(){
+    console.log(`DB Connected`);
+})()
+
+
+
+
+
+
+
+
+
