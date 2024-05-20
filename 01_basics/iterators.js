19f2303fb96f117b4f20c2ea31de7e018c534413
rohitbhatt2437 <136969234+rohitbhatt2437@users.noreply.github.com>
2024-05-20 19:55:22 +0000

    done with iterators and higher level iterators

---
+++ 01_basics/iterators.js
@@ -0,0 +1,52 @@
+// for(let i =0; i< 10; i++){
+//     let element = i
+//     console.log(element);
+// }
+
+// console.log(element);
+
+
+/////////////////////HIGHER ORDER LOOP//////////////////////
+// for of
+
+// const arr = [1,2,3,4,5,6]
+// for(const i of arr){
+//     console.log(i);
+// }
+
+// const greetings = "Hello world"
+// for(const greet of greetings){
+//     if(greet===' '){
+//         continue
+//     }
+//     else{
+//         console.log(`The char is ${greet}`);
+//     }
+// }
+
+
+
+// MAPS
+// const map = new Map()
+// map.set('IN', 'INDIA')
+// map.set('US', "UNITED STATES")
+// map.set('FR', 'FRANCE')
+
+// for(const [key, value] of map){
+//     console.log(key);
+// }
+
+
+// FOR EACH
+const languages = ["python", "rust", "ruby", "swift", "c++", "js"]
+
+languages.forEach(function(value){
+    console.log(value);
+})
+
+
+
+
+
+
+
